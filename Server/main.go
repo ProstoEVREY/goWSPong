ea7a3813c8e2594fcc9858e4c81445ed7c91b0d4
Dmitriy Katkov <dmitriy.katkov.dk@gmail.com>
2023-12-02 10:25:23 +0500

    IT WORKS

--- Server/main.go
+++ Server/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 	"sync"
+	"time"
 )
 
 // Player represents a connected player.
@@ -19,9 +20,8 @@ type Player struct {
 // GameServer represents the game server.
 
 type GameServer struct {
-	players      map[int]Player
+	players      map[int]*Player
 	broadcast    chan []byte
-	bdBall       chan []byte
 	mutex        sync.Mutex
 	upgrader     websocket.Upgrader
 	canvasHeight int
@@ -41,21 +41,6 @@ func (gs *GameServer) run() {
 		case message := <-gs.broadcast:
 			gs.mutex.Lock()
 			for index, player := range gs.players {
-				println(string(message))
-				err := player.conn.WriteMessage(websocket.TextMessage, message)
-				if err != nil {
-					log.Println("Error writing to player:", err)
-					err := player.conn.Close()
-					if err != nil {
-						return
-					}
-					delete(gs.players, index)
-				}
-			}
-			gs.mutex.Unlock()
-		case message := <-gs.bdBall:
-			gs.mutex.Lock()
-			for index, player := range gs.players {
 				err := player.conn.WriteMessage(websocket.TextMessage, message)
 				if err != nil {
 					log.Println("Error writing to player:", err)
@@ -68,34 +53,46 @@ func (gs *GameServer) run() {
 			}
 			gs.mutex.Unlock()
 		}
-
 	}
 }
 
 func (gs *GameServer) controlBall() {
 	for {
+		time.Sleep(10 * time.Millisecond)
+
 		gs.ball.X += gs.ball.VelocityX
 		gs.ball.Y += gs.ball.VelocityY
 
 		gs.checkPaddleCollision()
 
-		gs.bdBall <- gs.serializeBallState()
+		gs.broadcast <- gs.serializeGameState()
+
 	}
 }
 
 func (gs *GameServer) checkPaddleCollision() {
-	for _, player := range gs.players {
+	gs.mutex.Lock()
+	defer gs.mutex.Unlock()
+
+	// Create a copy of the players map to avoid concurrent modification
+	playersCopy := make(map[int]Player, len(gs.players))
+	for key, value := range gs.players {
+		playersCopy[key] = *value
+	}
+
+	for _, player := range playersCopy {
 
 		if (gs.ball.X-10 <= 35 || gs.ball.X+10 >= gs.canvasWidth-35) && gs.ball.Y >= player.positionY && gs.ball.Y <= player.positionY+100 {
 			gs.ball.VelocityX = -gs.ball.VelocityX
 		}
-		if gs.ball.Y-10 <= 0 || gs.ball.Y+10 >= gs.canvasHeight {
-			gs.ball.VelocityY = -gs.ball.VelocityY
-		}
+	}
 
-		if gs.ball.X-10 <= 0 || gs.ball.X+10 >= gs.canvasWidth {
-			gs.ball.VelocityX = -gs.ball.VelocityX
-		}
+	if gs.ball.Y-10 <= 0 || gs.ball.Y+10 >= gs.canvasHeight {
+		gs.ball.VelocityY = -gs.ball.VelocityY
+	}
+
+	if gs.ball.X-10 <= 0 || gs.ball.X+10 >= gs.canvasWidth {
+		gs.ball.VelocityX = -gs.ball.VelocityX
 	}
 
 }
@@ -121,7 +118,7 @@ func (gs *GameServer) handleConnection(w http.ResponseWriter, r *http.Request) {
 	}
 
 	gs.mutex.Lock()
-	gs.players[index] = *player
+	gs.players[index] = player
 	gs.mutex.Unlock()
 
 	for {
@@ -140,21 +137,22 @@ func (gs *GameServer) handleConnection(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 
-		print(actionMessage["action"])
 		switch actionMessage["action"] {
 		case "moveUp":
-			player.positionY -= 20
+			player.positionY -= 30
 			if player.positionY < 0 {
 				player.positionY = 0
 			}
 		case "moveDown":
-			player.positionY += 20
+			player.positionY += 30
 			if player.positionY > 500 {
 				player.positionY = 500
 			}
 		case "stopMove":
 			// Handle stopping player movement
 		}
+
+		println(player.positionY)
 
 		// Broadcast updated player positions to all clients
 		gs.broadcast <- gs.serializeGameState()
@@ -172,20 +170,6 @@ func (gs *GameServer) serializeGameState() []byte {
 		gameState[insert] = player.positionY
 	}
 
-	jsonData, err := json.Marshal(gameState)
-	if err != nil {
-		log.Println("Error encoding JSON:", err)
-		return nil
-	}
-
-	return jsonData
-}
-
-func (gs *GameServer) serializeBallState() []byte {
-	gs.mutex.Lock()
-	defer gs.mutex.Unlock()
-
-	gameState := make(map[string]int)
 	gameState["ballX"] = gs.ball.X
 	gameState["ballY"] = gs.ball.Y
 
@@ -201,11 +185,10 @@ func (gs *GameServer) serializeBallState() []byte {
 func main() {
 
 	gs := &GameServer{
-		players:      make(map[int]Player),
+		players:      make(map[int]*Player),
 		canvasHeight: 600,
 		canvasWidth:  800,
 		broadcast:    make(chan []byte),
-		bdBall:       make(chan []byte),
 		upgrader: websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
 				return true
